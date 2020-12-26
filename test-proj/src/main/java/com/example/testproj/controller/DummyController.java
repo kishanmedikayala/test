ac4e5abdb2f6f9f276370a6389a6b797862c06c7
kishanmedikayala <kishanmdkyl8@gmail.com>
2020-12-26 15:36:05 +0530

    api added

---
+++ test-proj/src/main/java/com/example/testproj/controller/DummyController.java
@@ -0,0 +1,13 @@
+package com.example.testproj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DummyController {
+	@GetMapping("/ping")
+	public String getData() {
+		return "KL";
+	}
+
+}
